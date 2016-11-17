e3b55eff01fab27fbe4ed5d8a3d23018b8ed31c8
Henry Acosta <you@example.com>
2016-11-17 10:20:31 -0500

    final commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title:'Javascript: The Good Parts', pages: 150},
+    {title:'Javascript: The Bad Parts', pages: 350},
+    {title:'Javascript: The Ugly Parts', pages: 250},
+    {title:'Javascript: The Worst Parts', pages: 450}
+  ]
+}
+ {/*this is our books reducer*/}
